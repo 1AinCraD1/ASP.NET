81962f38e83cd599d42221d1fe92e5e3b0f4fcbe
1AinCraD1 <118106610+1AinCraD1@users.noreply.github.com>
2024-12-14 12:55:51 +0200

    Create AppDbContext.cs

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Namespace.Models;
+
+namespace Namespace.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products => Set<Product>();
+    }
+}
